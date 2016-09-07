c27951cb6f6e375237deaf5865d8bc449d609ae7
dahrapucher <dahra.pucher@gmail.com>
2016-09-07 14:22:28 -0700

    added files and notes

---
+++ APLesson_02/Lab_02.java
@@ -0,0 +1,22 @@
+public class Lab_02
+{
+	public static void main (String[]args)
+	{
+		int A = 5;
+		int B = 9;
+		int multiplication = A * B;
+		System.out.println("5 multiplied by 9 is" + " " + multiplication);
+		String name = "Dahra Pucher";
+		System.out.println(name);
+		String address = "4991 Gunston Court";
+		System.out.println(address);
+		String city = "San Diego, CA";
+		String zip = "92130";
+		System.out.println(city + " " + zip);
+		int w = 5;
+		int l = 3;
+		int h = 7;
+		int surfacearea = 2 * (w * l + h * l + h * w);
+		System.out.println("The surface area of your rectangle is" + " " + surfacearea);
+	}
+}
