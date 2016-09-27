82b89d4430eeb0a93b140fbb6ffd9641456e9dbb
dahrapucher <dahra.pucher@gmail.com>
2016-09-27 14:29:33 -0700

    worked on lesson 4

---
+++ APLESSON_04/Rectangle.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Rectangle 
+{
+	static double l;
+	static double w;
+	static double perimeter;
+	
+	public static void main(String[]args);
+	{
+		Scanner kb = new Scanner(System.in);
+		System.out.println("Enter length: ");
+		l = kb.nextDouble();
+		System.out.println("Enter width: ");
+		w = kb.nextDouble();
+		
+		calcPerim(double l, double w);
+		print();
+		
+		
+	}
+	public static void calcPerim(double l, double w)
+	{
+		return 2*w+2*l
+		perimeter = return
+	}
+	
+	public static void print
+	{
+		System.out.println
+	}
+}
