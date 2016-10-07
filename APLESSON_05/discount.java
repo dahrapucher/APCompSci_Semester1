a2a439ba4fb60ad7653acbddeac01677b87945e2
dahrapucher <dahra.pucher@gmail.com>
2016-10-07 13:34:18 -0700

    finished lesson 5

--- APLESSON_05/discount.java
+++ APLESSON_05/discount.java
@@ -13,16 +13,19 @@ public class discount
 		String item1 = kb.nextLine();
 		System.out.println("Enter the price of the first item:");
 		double price1 = kb.nextDouble();
+		kb.nextLine();//soaks up extra return key
 		//user input for item2
 		System.out.println("Enter second item: ");
 		String item2 = kb.nextLine();
 		System.out.println("Enter the price of the second item: ");
 		double price2 = kb.nextDouble();
+		kb.nextLine();//soaks up extra return key
 		//user input for item3
 		System.out.println("Enter third item:");
-		String itme3 = kb.nextLine();
+		String item3 = kb.nextLine();
 		System.out.println("Enter the price of the third item:");
 		double price3 = kb.nextDouble();
+		kb.nextLine();//soaks up extra return key
 		//user input for item4
 		System.out.println("Enter fourth item:");
 		String item4 = kb.nextLine();
@@ -30,40 +33,40 @@ public class discount
 		double price4 = kb.nextDouble();
 		
 		double sub = price1+price2+price3+price4;
-		String discount = discount(sub);
-		double total = sub - dicount + tax;
+		double discnt = discount(sub);
+		double total = sub - discnt + sub*.075;
 		
 		//format reciept
-		System.out.println("<<<<<<<<<<_Receipt_>>>>>>>>>>");
+		System.out.println(" <<<<<<<<<<<<<_Receipt_>>>>>>>>>>>>>");
 		format(item1, price1);
 		format(item2, price2);
 		format(item3, price3);
 		format(item4, price4);
 		System.out.println("\n");
 		format("Subtotal: ", sub);
-		format("Discount:", discount);
+		format("Discount:", discnt);
 		format("Total: ", total);
 		System.out.println("\n");
-		System.out.println("-----------------------------------");
+		System.out.println("----------------------------------------");
 		System.out.println("\t*  Thank you for your support  *");
 		
 	}
 	
-	public static boolean discount(double total, String discount)
+	public static double discount(double total)
 	{
-		double discount = 0;
+		double disc = 0;
 		if(total >= 2000)
 		{
-			discount = total * 0.15;
+			disc = total * 0.15;
 		}
 		if(total < 2000)
 		{
-			discount = 0;
+			disc = 0;
 		}
-		return discount;
+		return disc;
 	}
 	
-	public void format(String item, double price)
+	public static void format(String item, double price)
 	{
 		System.out.printf("\n*%20s ........ %7.2f", item, price);
 	}
