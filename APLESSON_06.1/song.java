84aa9bcf4ed24200e73e1647eb711802321f7fdf
dahrapucher <dahra.pucher@gmail.com>
2016-10-27 13:04:16 -0700

    finished lesson 6.1

--- APLESSON_06.1/song.java
+++ APLESSON_06.1/song.java
@@ -3,14 +3,17 @@ public class song
 	public static void main(String[]args)
 	{
 		sing("Na", 4);
+		System.out.println("\n");
 		sing("Na", 4);
+		System.out.println("\n");
 		sing("Hey", 3);
+		System.out.println("\n");
 		sing("Goodbye", 1);
 	}
 	
 	public static void sing(String word, int num)
 	{
 		for(int i = 1; i <= num; i++)
-			System.out.println(word + " ");
+			System.out.printf(word + " ");
 	}
 }
