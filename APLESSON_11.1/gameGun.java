479206f970703240dce85728d9864b43d225cfca
dahrapucher <dahra.pucher@gmail.com>
2016-12-14 14:32:23 -0800

    worked on lab 11.1

---
+++ APLESSON_11.1/gameGun.java
@@ -0,0 +1,81 @@
+import java.util.Scanner;
+
+public class gameGun
+{
+	static int bulletCount;
+	static int clipSize = 16;
+	static int shotCount;
+	static String[] clip;
+	
+	public static void main(String[]args)
+	{
+		Scanner kb = new Scanner(System.in);
+		bulletCount = 96;
+		shotCount = 0;
+		clip = new String[clipSize];
+		
+		resetClip();
+		
+		while(bulletCount > 0 || shotCount > 0)
+		{
+			System.out.println("Action(R or S):");
+			String action = kb.next();
+			if(action.equals("R"))
+				reload();
+			if(action.equals("S"))
+				System.out.println(shoot());
+		}
+		printClip();
+	}
+	
+	public static void resetClip()
+	{
+		for(int i = 0; i < clip.length; i++)
+			clip[i] = "[]";
+	}
+	
+	public static String shoot()
+	{
+		if(shotCount > 0)
+		{
+			clip[shotCount - 1] = "[]";
+			shotCount -= 1;
+			return "Boom!!";
+		}
+		else
+		{
+			return "Reload!!";
+		}
+	}
+	
+	public static void reload()
+	{
+		if(bulletCount >= clipSize)
+		{
+			bulletCount -= clipSize;
+			shotCount = clipSize;
+		}
+		else
+		{
+			shotCount = bulletCount;
+			bulletCount = 0;
+		}
+		resetClip();
+		
+		for(int i = 0; i < shotCount; i++)
+		{
+			clip[i] = " * ";
+		}
+	}
+	
+	public static void printClip()
+	{
+		String output = "";
+		output += "Bullets:\t" + bulletCount + "\n" + "Clip:\t";
+		for(int i = 0; i < clipSize; i++)
+		{
+			output += clip[i];
+		}
+		System.out.println(output);
+	}
+}
---
+++ APLESSON_11.1/gameGun_2.java
@@ -0,0 +1,68 @@
+import java.util.Scanner;
+
+public class gameGun_2
+{
+	static String[]health;
+	static int healthLoad = 6;
+	static int healthCount;
+	
+	public static void main(String[]args)
+	{
+		Scanner kb = new Scanner(System.in);
+		String turn = "";
+		int damage = 0;
+		int amount = 0;
+		healthCount = 6;
+		health = new String[healthLoad];
+		
+		while(turn != "Q" && healthCount > 0)
+		{
+			System.out.println("Your turn! Hit Enter when ready:");
+			turn = kb.next();
+			damage = (int)(Math.random()*2) + 1;
+			amount = (int)(Math.random()*6) + 1;
+		}
+		
+		printClip();
+		System.out.println("You died!!");
+	}
+	
+	public static String takeDamage(int damage, int amount)
+	{
+		if(damage == 1)
+		{
+			healthCount -= amount;
+			return "Taking " + amount + " damage!";
+		}
+		else
+		{
+			if(healthCount + amount < healthLoad)
+			{
+				healthCount += amount;
+			}
+			else
+			{
+				healthCount = healthLoad;
+			}
+			return "Power up " + amount + "!";
+		}
+	}
+	
+	public static void printClip()
+	{
+		String output = "Health\t";
+		for(int i = 0; i < healthLoad; i++)
+		{
+			if(i < healthCount)
+			{
+				health[i] = "@";
+			}
+			else
+			{
+				health[i] = "[]";
+			}
+			output += health[i];
+		}
+		System.out.println(output);
+	}
+}
